8c6e1d961ad2b2248b988507b3ca63222c84aef7
Elonita Krasniqi <elonita.krasniqi@student.uni-pr.edu>
2023-11-08 18:45:54 +0100

    Finished!

--- Files/script.js
+++ Files/script.js
@@ -3,6 +3,5 @@ const imageContainer = document.querySelector(".img-container");
 imageContainer.addEventListener("mouseout", function() {
     imageContainer.style.transform = "scale(1)"; 
 });
-
-
+ 
 
